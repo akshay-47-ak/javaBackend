39d2ca178f345129c4f9f58240857caabeffad21
Akshay <akshayathavale1525@gmail.com>
2025-11-03 22:48:50 +0530

    Todays work

---
+++ springCore/src/main/java/com/springCore/collections/emp1.java
@@ -0,0 +1,77 @@
+package com.springCore.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class emp1 {
+
+	private int id;
+	private String name;
+	
+	private List<String> phones;
+	private Set<String> addresses;
+	private Map<String, String> courses;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<String> getPhones() {
+		return phones;
+	}
+	
+	public void setPhones(List<String> phones) {
+		this.phones = phones;
+	}
+	
+	public Set<String> getAddresses() {
+		return addresses;
+	}
+	
+	public void setAddresses(Set<String> addresses) {
+		this.addresses = addresses;
+	}
+	
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+	
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+
+	
+	public emp1(int id, String name, List<String> phones, Set<String> addresses, Map<String, String> courses) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phones = phones;
+		this.addresses = addresses;
+		this.courses = courses;
+	}
+
+	public emp1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	
+}
